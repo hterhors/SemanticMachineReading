c36f2f854203b23f53bce1d480d6b0decf770636
hterhors <terhorsthendrik@gmail.com>
2019-04-26 10:33:52 +0200

    add preprocessing stuff

---
+++ src/main/java/de/hterhors/semanticmr/examples/olp2/corpus/TextReader.java
@@ -0,0 +1,64 @@
+package de.hterhors.semanticmr.examples.olp2.corpus;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Easy and (very) dirty text reader!
+ * 
+ * @author hterhors
+ *
+ */
+public class TextReader {
+	public static void main(String[] args) throws IOException {
+		TextReader crr = new TextReader();
+
+//		crr.de_textMap.entrySet().forEach(System.out::println);
+		crr.en_textMap.entrySet().forEach(System.out::println);
+	}
+
+	private final Pattern textPattern = Pattern.compile("<bodyelem id=\"(.+?)\">(.*?)</bodyelem>");
+	private final Pattern idPattern = Pattern.compile("<document name=\"(.*?)\">");
+	private final Pattern titlePattern = Pattern.compile("<title>(.*?)</title>");
+
+	public final Map<String, String> de_textMap = new HashMap<>();
+	public final Map<String, String> en_textMap = new HashMap<>();
+
+	public TextReader() throws IOException {
+
+		File crossRefDir = new File("data/Text/");
+
+		for (File crossRefFile : crossRefDir.listFiles()) {
+
+			String doc = Files.readAllLines(crossRefFile.toPath()).stream().reduce(String::concat).get();
+
+			Matcher idp = idPattern.matcher(doc);
+			Matcher titp = titlePattern.matcher(doc);
+
+			if (idp.find() && titp.find()) {
+				final String id = idp.group(1);
+
+				Matcher mT = textPattern.matcher(doc);
+				String text = titp.group(1).trim() + ". ";
+
+				while (mT.find()) {
+
+					text += mT.group(2).trim() + " ";
+
+				}
+				text = text.trim();
+				if (id.startsWith("de_")) {
+					de_textMap.put(id, text);
+				} else if (id.startsWith("en_")) {
+					en_textMap.put(id, text);
+				}
+			}
+		}
+	}
+
+}
