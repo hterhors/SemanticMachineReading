a596204813017ef249f32a0441b3ebb93b163e28
Hendrik <hterhors@techfak.uni-bielefeld.de>
2019-06-21 14:50:07 +0200

    under dev

---
+++ src/main/java/de/hterhors/semanticmr/crf/exploration/RootTemplateCardinalityExplorer.java
@@ -0,0 +1,107 @@
+package de.hterhors.semanticmr.crf.exploration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import de.hterhors.semanticmr.candprov.sf.AnnotationCandidateRetrievalCollection;
+import de.hterhors.semanticmr.candprov.sf.IEntityTypeAnnotationCandidateProvider;
+import de.hterhors.semanticmr.crf.of.IObjectiveFunction;
+import de.hterhors.semanticmr.crf.structure.annotations.AbstractAnnotation;
+import de.hterhors.semanticmr.crf.structure.annotations.AnnotationBuilder;
+import de.hterhors.semanticmr.crf.structure.annotations.EntityTemplate;
+import de.hterhors.semanticmr.crf.structure.annotations.EntityTypeAnnotation;
+import de.hterhors.semanticmr.crf.variables.IStateInitializer;
+import de.hterhors.semanticmr.crf.variables.State;
+
+/**
+ * @author hterhors
+ *
+ */
+public class RootTemplateCardinalityExplorer implements IExplorationStrategy {
+	private static Logger log = LogManager.getFormatterLogger(RootTemplateCardinalityExplorer.class);
+
+	public static int MAX_NUMBER_OF_ANNOTATIONS;
+
+	final private AnnotationCandidateRetrievalCollection candidateProvider;
+
+	final private IObjectiveFunction objectiveFunction;
+
+	final private EntityTypeAnnotation initAnnotation;
+
+	public RootTemplateCardinalityExplorer(IObjectiveFunction objectiveFunction,
+			AnnotationCandidateRetrievalCollection candidateProvider, EntityTypeAnnotation initAnnotation) {
+		this.candidateProvider = candidateProvider;
+		this.objectiveFunction = objectiveFunction;
+		this.initAnnotation = initAnnotation;
+	}
+
+	/**
+	 * Average number of new explored proposal states. This variable is used as
+	 * initial size of the next new proposal state list.
+	 */
+	int averageNumberOfNewProposalStates = 16;
+
+	@Override
+	public List<State> explore(State currentState) {
+
+		final List<State> proposalStates = new ArrayList<>(averageNumberOfNewProposalStates);
+
+		EntityTypeAnnotation init = initAnnotation.deepCopy();
+
+		for (IEntityTypeAnnotationCandidateProvider slotFillerCandidateProvider : candidateProvider
+				.getEntityTypeCandidateProvider(currentState.getInstance())) {
+
+			for (EntityTypeAnnotation templateTypeCandidate : slotFillerCandidateProvider
+					.getCandidates(init.getEntityType())) {
+
+				if (templateTypeCandidate.equals(init))
+					continue;
+
+				proposalStates.add(currentState.deepAddCopy(init));
+			}
+
+		}
+
+		for (int annotationIndex = 0; annotationIndex < currentState.getCurrentPredictions().getAnnotations()
+				.size(); annotationIndex++) {
+
+			proposalStates.add(currentState.deepRemoveCopy(annotationIndex));
+
+		}
+
+		if (proposalStates.isEmpty()) {
+			log.warn("No states were generated for instance: " + currentState.getInstance().getName());
+		}
+
+		proposalStates.forEach(System.out::println);
+
+		updateAverage(proposalStates);
+		return proposalStates;
+
+	}
+
+	private void updateAverage(final List<State> proposalStates) {
+		averageNumberOfNewProposalStates += proposalStates.size();
+		averageNumberOfNewProposalStates /= 2;
+	}
+
+	private void changeTemplateType(final List<State> proposalStates, State currentState,
+			IEntityTypeAnnotationCandidateProvider slotFillerCandidateProvider, EntityTemplate entityTemplate,
+			int annotationIndex) {
+
+		for (EntityTypeAnnotation templateTypeCandidate : slotFillerCandidateProvider
+				.getCandidates(entityTemplate.getEntityType())) {
+
+			if (templateTypeCandidate.equals(entityTemplate.getRootAnnotation()))
+				continue;
+
+			final EntityTemplate deepCopy = entityTemplate.deepMergeCopy(templateTypeCandidate);
+
+			proposalStates.add(currentState.deepUpdateCopy(annotationIndex, deepCopy));
+		}
+	}
+
+}
