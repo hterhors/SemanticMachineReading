bfa93b50f8a4cc9befd3aa789587ac08a3dba780
Hendrik <hterhors@techfak.uni-bielefeld.de>
2019-11-05 17:16:49 +0100

    minor change

--- src/main/java/de/hterhors/semanticmr/crf/model/AbstractFactorScope.java
+++ src/main/java/de/hterhors/semanticmr/crf/model/AbstractFactorScope.java
@@ -81,29 +81,5 @@ public abstract class AbstractFactorScope {
 		return "FactorScope [template=" + template + "]";
 	}
 
-//	@Override
-//	public int hashCode() {
-//		final int prime = 31;
-//		int result = 1;
-//		result = prime * result + ((template == null) ? 0 : template.hashCode());
-//		return result;
-//	}
-//
-//	@Override
-//	public boolean equals(Object obj) {
-//		if (this == obj)
-//			return true;
-//		if (obj == null)
-//			return false;
-//		if (getClass() != obj.getClass())
-//			return false;
-//		AbstractFactorScope other = (AbstractFactorScope) obj;
-//		if (template == null) {
-//			if (other.template != null)
-//				return false;
-//		} else if (!template.equals(other.template))
-//			return false;
-//		return true;
-//	}
 
 }
